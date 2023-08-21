c27f6206432a8e72551a41a5a06e01867592529a
Clément Boudereau <cboudereau@gmail.com>
2023-08-21 11:29:11 +0200

    clean

--- java/src/main/java/com/dataseries/DataPoints.java
+++ java/src/main/java/com/dataseries/DataPoints.java
@@ -6,16 +6,16 @@ import java.util.stream.StreamSupport;
 import java.util.Iterator;
 
 public class DataPoints {
-    public static <P, T> DataPoint<P, T> datapoint(final P point, final T data) {
+    public static final <P, T> DataPoint<P, T> datapoint(final P point, final T data) {
         return new DataPoint<>(point, data);
     }
 
-    public static <P, T> Stream<DataPoint<P, T>> stream(final Iterator<DataPoint<P, T>> iterator) {
+    public static final <P, T> Stream<DataPoint<P, T>> stream(final Iterator<DataPoint<P, T>> iterator) {
         Iterable<DataPoint<P, T>> iterable = () -> iterator;
         return StreamSupport.stream(iterable.spliterator(), false);
     }
 
-    public static <P extends Comparable<P>, L, R, T> Iterator<DataPoint<P, T>> union(
+    public static final <P extends Comparable<P>, L, R, T> Iterator<DataPoint<P, T>> union(
             final Iterator<DataPoint<P, L>> left, final Iterator<DataPoint<P, R>> right,
             final Function<UnionResult<L, R>, T> f) {
         return new Union<P, L, R, T>(left, right, f);
