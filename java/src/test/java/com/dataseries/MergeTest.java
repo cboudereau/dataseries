bfebadd8c5120059374a5c19fef9219004ced3f9
Clément Boudereau <cboudereau@gmail.com>
2023-08-22 19:01:57 +0200

    clean

--- java/src/test/java/com/dataseries/MergeTest.java
+++ java/src/test/java/com/dataseries/MergeTest.java
@@ -1,9 +1,6 @@
 package com.dataseries;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
 import java.util.Optional;
 import java.util.List;
 import org.junit.jupiter.api.Test;
