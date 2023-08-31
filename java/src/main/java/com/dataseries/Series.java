87c1fcef1281a000079143d778b64217bcba660f
Clément Boudereau <cboudereau@gmail.com>
2023-08-31 14:42:32 +0200

    java/feat: add release workflow (#19)

--- java/src/main/java/com/dataseries/Series.java
+++ java/src/main/java/com/dataseries/Series.java
@@ -2,17 +2,50 @@ package com.dataseries;
 
 import java.util.function.Function;
 
+/**
+ * The entrypoint of the api
+ */
 public class Series {
+    private Series () {
+
+    }
+    
+    /**
+     * a helper function to create a datapoint
+     * @param <P> the point type
+     * @param <T> the data type
+     * @param point the point
+     * @param data the data
+     * @return a datapoint
+     */
     public static final <P, T> DataPoint<P, T> datapoint(final P point, final T data) {
         return new DataPoint<>(point, data);
     }
 
+    /**
+     * union 2 series and combine union result with the given function
+     * @param <P> the point type should be common for left and right series
+     * @param <L> the left type
+     * @param <R> the right type
+     * @param <T> the return of the applied function to union result
+     * @param left the left serie
+     * @param right the right serie
+     * @param f the function applied to convert union result to T type
+     * @return a iterable series
+     */
     public static final <P extends Comparable<P>, L, R, T> IterableSeries<P, T> union(
             final Iterable<DataPoint<P, L>> left, final Iterable<DataPoint<P, R>> right,
             final Function<UnionResult<L, R>, T> f) {
         return () -> new Union<>(left.iterator(), right.iterator(), f);
     }
 
+    /**
+     * merge a serie to be more compact when contigous events have the same data
+     * @param <P> the point type
+     * @param <T> the data type
+     * @param series the series to merge
+     * @return a merged series which have no more duplicated events for the same data
+     */
     public static final <P, T> IterableSeries<P, T> merge(
             final Iterable<DataPoint<P, T>> series) {
         return () -> new Merge<>(series.iterator());
--- java/src/main/java/com/dataseries/UnionResult.java
+++ java/src/main/java/com/dataseries/UnionResult.java
@@ -1,26 +1,80 @@
 package com.dataseries;
 
-public sealed interface UnionResult<L, R>
-        permits UnionResult.LeftOnly, UnionResult.RightOnly, UnionResult.Both {
+/**
+ * The Union result algebraic data type (sum type)
+ * 
+ * @param <R> the right data
+ * @param <L> the left data
+ */
+public sealed interface UnionResult<L, R> permits UnionResult.LeftOnly, UnionResult.RightOnly, UnionResult.Both {
+    /**
+     * Both record
+     * 
+     * @param <L>   the left type
+     * @param <R>   the right type
+     * @param right the right value
+     * @param left  the left value
+     */
     public static final record Both<L, R>(L left, R right) implements UnionResult<L, R> {
 
     }
 
+    /**
+     * Left only record
+     * 
+     * @param <L>  the left type
+     * @param <R>  the right type
+     * @param left the left value
+     */
     public static final record LeftOnly<L, R>(L left) implements UnionResult<L, R> {
     }
 
+    /**
+     * Right only record
+     * 
+     * @param <L>   the left type
+     * @param <R>   the right type
+     * @param right the right value
+     */
     public static final record RightOnly<L, R>(R right) implements UnionResult<L, R> {
 
     }
 
+    /**
+     * A left only union result when there is no right data for the given point
+     * 
+     * @param <L>  the left type
+     * @param <R>  the right type
+     * @param left the left data
+     * @return A left only union result when there is no right data for the given
+     *         point
+     */
     public static <L, R> UnionResult.LeftOnly<L, R> leftOnly(L left) {
         return new UnionResult.LeftOnly<L, R>(left);
     }
 
+    /**
+     * A right only union result when there is no left data for the given point
+     * 
+     * @param <L>   the left type
+     * @param <R>   the right type
+     * @param right the right data
+     * @return A right only union result when there is no left data for the given
+     *         point
+     */
     public static <L, R> UnionResult.RightOnly<L, R> rightOnly(R right) {
         return new UnionResult.RightOnly<L, R>(right);
     }
 
+    /**
+     * A union of both (left and right)
+     * 
+     * @param <L>   the left type
+     * @param <R>   the right type
+     * @param left  the left data
+     * @param right the right data
+     * @return A union of both (left and right)
+     */
     public static <L, R> UnionResult.Both<L, R> both(L left, R right) {
         return new UnionResult.Both<L, R>(left, right);
     }
