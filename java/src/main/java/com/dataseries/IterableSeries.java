efeb7a135c5b2a7b5c502fcacdf4b2af1d6916ea
Clément Boudereau <cboudereau@gmail.com>
2023-08-31 13:32:19 +0200

    java: setup javadoc

--- java/src/main/java/com/dataseries/IterableSeries.java
+++ java/src/main/java/com/dataseries/IterableSeries.java
@@ -4,6 +4,9 @@ import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public interface IterableSeries<P, T> extends Iterable<DataPoint<P, T>> {
+    /**
+     * Convert to a conventional stream
+     */
     public default Stream<DataPoint<P, T>> stream() {
         return StreamSupport.stream(this.spliterator(), false);
     }
