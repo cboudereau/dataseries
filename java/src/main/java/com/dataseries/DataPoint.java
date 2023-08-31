9eab66fdf6b17739bfde0a52b091bbb60404c20a
Clément Boudereau <cboudereau@gmail.com>
2023-08-31 14:18:33 +0200

    java: add java doc

--- java/src/main/java/com/dataseries/DataPoint.java
+++ java/src/main/java/com/dataseries/DataPoint.java
@@ -1,5 +1,13 @@
 package com.dataseries;
 
+/**
+ * Data point record to store data at any given point.
+ * 
+ * @param <P> the point type
+ * @param <T> the data type
+ * @param point the point value
+ * @param data the data value
+ */
 public record DataPoint<P, T>(P point, T data) {
 
 }
--- java/src/main/java/com/dataseries/IterableSeries.java
+++ java/src/main/java/com/dataseries/IterableSeries.java
@@ -3,9 +3,16 @@ package com.dataseries;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+/**
+ * IterableSeries wrap DataPoint and have a default useful method to use with the java stream api
+ * 
+ * @param <P> the point type
+ * @param <T> the data type
+ */
 public interface IterableSeries<P, T> extends Iterable<DataPoint<P, T>> {
     /**
      * Convert to a conventional stream
+     * @return a datapoint stream
      */
     public default Stream<DataPoint<P, T>> stream() {
         return StreamSupport.stream(this.spliterator(), false);
