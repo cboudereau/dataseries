a4027166768a4ff9173e354c8baf88bcb27abb63
Clément Boudereau <cboudereau@gmail.com>
2023-09-06 19:17:39 +0200

    add intersection example

--- java/src/test/java/io/github/cboudereau/dataseries/snippets/SamplesTest.java
+++ java/src/test/java/io/github/cboudereau/dataseries/snippets/SamplesTest.java
@@ -8,13 +8,8 @@ import org.junit.jupiter.api.Test;
 
 import io.github.cboudereau.dataseries.Series;
 import io.github.cboudereau.dataseries.UnionResult;
-import io.github.cboudereau.dataseries.UnionResult.Both;
-import io.github.cboudereau.dataseries.UnionResult.RightOnly;
 
 public class SamplesTest {
-    private static record Tuple<L, R>(L fst, R snd) {
-    }
-
     @Test
     public void simple() {
         var s1 = List.of(Series.datapoint(3, 50));
@@ -30,6 +25,24 @@ public class SamplesTest {
         assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void intersection() {
+        var s1 = List.of(Series.datapoint(3, 50));
+        var s2 = List.of(Series.datapoint(4, 100), Series.datapoint(7, 110));
+
+        var actual = Series.union(s1, s2, SamplesTest::toTuple).stream().filter(x -> x.data().isPresent())
+                .map(x -> Series.datapoint(x.point(), x.data().get())).toArray();
+
+        var expected = List.of(
+                Series.datapoint(4, new Tuple<>(50, 100)),
+                Series.datapoint(7, new Tuple<>(50, 110))).toArray();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    private static record Tuple<L, R>(L fst, R snd) {
+    }
+
     private static <L, R> Optional<Tuple<L, R>> toTuple(UnionResult<L, R> unionResult) {
         switch (unionResult) {
             case UnionResult.LeftOnly<L, R> x -> {
@@ -43,19 +56,5 @@ public class SamplesTest {
                 return Optional.of(new Tuple<L, R>(both.left(), both.right()));
             }
         }
-    }
-
-    @Test
-    public void intersection() {
-        var s1 = List.of(Series.datapoint(3, 50));
-        var s2 = List.of(Series.datapoint(4, 100), Series.datapoint(7, 110));
-
-        var actual = Series.union(s1, s2, SamplesTest::toTuple).stream().filter(x -> x.data().isPresent()).map(x -> Series.datapoint(x.point(), x.data().get())).toArray();
-
-        var expected = List.of(
-                Series.datapoint(4, new Tuple<>(50, 100)),
-                Series.datapoint(7, new Tuple<>(50, 110))).toArray();
-
-        assertArrayEquals(expected, actual);
     }
 }
