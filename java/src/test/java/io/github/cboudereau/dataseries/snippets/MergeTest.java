27ed4976168e9daf4527d999c30904196cb99585
Clément Boudereau <cboudereau@gmail.com>
2023-09-07 13:15:23 +0200

    java/example : add merge example

--- java/src/test/java/io/github/cboudereau/dataseries/snippets/MergeTest.java
+++ java/src/test/java/io/github/cboudereau/dataseries/snippets/MergeTest.java
@@ -2,6 +2,8 @@ package io.github.cboudereau.dataseries.snippets;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import io.github.cboudereau.dataseries.Series;
@@ -16,4 +18,15 @@ public class MergeTest {
 
         assertArrayEquals(expected.toArray(), actual.stream().toArray());
     }
+
+    @Test
+    public void uncontiguousTest() {
+        final var s1 = List.of(Series.datapoint(1, 100), Series.datapoint(3, 10));
+        final var actual = Series.merge(s1);
+
+        final var expected = List.of(Series.datapoint(1, 100), Series.datapoint(3, 10));
+
+        assertArrayEquals(expected.toArray(), actual.stream().toArray());
+    }
+
 }
