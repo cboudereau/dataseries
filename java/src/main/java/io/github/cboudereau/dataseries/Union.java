233d4e4a6ba58b3711efdc470dec354e2e1a003a
Clément Boudereau <cboudereau@gmail.com>
2023-09-08 13:36:40 +0200

    use switch expression

--- java/src/main/java/io/github/cboudereau/dataseries/Union.java
+++ java/src/main/java/io/github/cboudereau/dataseries/Union.java
@@ -130,6 +130,9 @@ final class Union<P extends Comparable<P>, L, R, T> implements Iterator<DataPoin
                     case final Cursor.Pair<T> pair when this.iterator.hasNext() ->
                         Optional.of(Cursor.pair(pair.second(), this.iterator.next()));
                     case final Cursor.Pair<T> pair -> Optional.of(Cursor.single(pair.second()));
+                    // FIXME : remove this when https://openjdk.org/jeps/433 will be ready (> 17,
+                    // java 20 at least)
+                    default -> throw new UnsupportedOperationException();
                 };
             }
 
